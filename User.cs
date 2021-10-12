ebd57ea355b38e70b7b717d198cd95c9e643de01
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-12 19:17:38 +0200

    Add project files.

---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp37
+{
+    class User
+    {
+        public int UserID { get; set; }
+    }
+}
---
+++ UserComtext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp37
+{
+    class UserComtext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE05;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+   
+        public UserComtext(DbContextOptions<UserComtext> options)
+           : base(options)
+        {
+        }
+    }
+}
