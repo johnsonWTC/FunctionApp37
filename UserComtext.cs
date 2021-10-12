d33f657e6a0186d89afb2c9635e57f80cf59fd91
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-12 19:43:05 +0200

    Functions

--- UserComtext.cs
+++ UserComtext.cs
@@ -9,7 +9,7 @@ namespace FunctionApp37
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE05;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FE05011;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
    
